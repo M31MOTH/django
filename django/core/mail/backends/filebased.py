d5cd9bc3595aa101e8ab1b7fb337ac9fc45b172d
Tom <tom@tomforb.es>
2017-07-21 22:15:37 +0100

    Ref #23919 - Replace os.path usages with pathlib.Path in filebased email backend

--- django/core/mail/backends/filebased.py
+++ django/core/mail/backends/filebased.py
@@ -2,6 +2,7 @@
 
 import datetime
 import os
+from pathlib import Path
 
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
@@ -13,29 +14,27 @@ from django.core.mail.backends.console import (
 class EmailBackend(ConsoleEmailBackend):
     def __init__(self, *args, file_path=None, **kwargs):
         self._fname = None
-        if file_path is not None:
-            self.file_path = file_path
-        else:
-            self.file_path = getattr(settings, 'EMAIL_FILE_PATH', None)
-        # Make sure self.file_path is a string.
-        if not isinstance(self.file_path, str):
-            raise ImproperlyConfigured('Path for saving emails is invalid: %r' % self.file_path)
-        self.file_path = os.path.abspath(self.file_path)
+        if file_path is None:
+            file_path = getattr(settings, 'EMAIL_FILE_PATH', None)
+        # Make sure self.file_path is a string or a pathlib object
+        if not isinstance(self.file_path, (str, Path)):
+            raise ImproperlyConfigured('Path for saving emails is invalid: %r' % file_path)
+        self.file_path = Path(file_path).absolute()
         # Make sure that self.file_path is an directory if it exists.
-        if os.path.exists(self.file_path) and not os.path.isdir(self.file_path):
+        if self.file_path.exists() and not self.file_path.is_dir():
             raise ImproperlyConfigured(
                 'Path for saving email messages exists, but is not a directory: %s' % self.file_path
             )
         # Try to create it, if it not exists.
-        elif not os.path.exists(self.file_path):
+        elif not self.file_path.exists():
             try:
-                os.makedirs(self.file_path)
+                self.file_path.mkdir(parents=True)
             except OSError as err:
                 raise ImproperlyConfigured(
                     'Could not create directory for saving email messages: %s (%s)' % (self.file_path, err)
                 )
         # Make sure that self.file_path is writable.
-        if not os.access(self.file_path, os.W_OK):
+        if not os.access(str(self.file_path), os.W_OK):
             raise ImproperlyConfigured('Could not write to directory: %s' % self.file_path)
         # Finally, call super().
         # Since we're using the console-based backend as a base,
@@ -53,12 +52,12 @@ class EmailBackend(ConsoleEmailBackend):
         if self._fname is None:
             timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
             fname = "%s-%s.log" % (timestamp, abs(id(self)))
-            self._fname = os.path.join(self.file_path, fname)
+            self._fname = self.file_path / fname
         return self._fname
 
     def open(self):
         if self.stream is None:
-            self.stream = open(self._get_filename(), 'ab')
+            self.stream = self._get_filename().open('ab')
             return True
         return False
 
